abb28f0885d0c0b3016eba16cfe9b7a60c70e3ff
shibboleth66 <38990987+shibboleth66@users.noreply.github.com>
2018-05-10 17:06:53 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,14 @@
 # wsgi.py
  
-from app import app
+from app import application
 from app import views
 import logging
  
 if __name__ != "__main__":
     
     gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
+    application.logger.handlers = gunicorn_logger.handlers
+    application.logger.setLevel(gunicorn_logger.level)
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
