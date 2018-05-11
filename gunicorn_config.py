849eb3206bdd3bd6f15005f048d0791d70120e0c
shibboleth66 <38990987+shibboleth66@users.noreply.github.com>
2018-05-11 18:19:06 +0100

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,10 @@
 import os
+import multiprocessing
 
-workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
+# bind = "127.0.0.1:8000"
+workers = multiprocessing.cpu_count() * 2 + 1
+
+# workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 
 forwarded_allow_ips = '*'
