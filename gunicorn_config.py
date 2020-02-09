dd0575e270d48a5d80fea6a04fdf612df94c8ed7
Ben Gallagher <shibboleth@me.com>
2020-02-09 16:11:48 +0000

    linting

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -6,4 +6,4 @@ workers = multiprocessing.cpu_count() * 2 + 1
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 
 forwarded_allow_ips = '*'
-secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+secure_scheme_headers = {'X-Forwarded-Proto': 'https'}
--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,11 @@
 # wsgi.py
- 
+
 from app import application
 from app import views
 import logging
- 
+
 if __name__ != "__main__":
-    
+
     gunicorn_logger = logging.getLogger('gunicorn.error')
     application.logger.handlers = gunicorn_logger.handlers
     application.logger.setLevel(gunicorn_logger.level)
